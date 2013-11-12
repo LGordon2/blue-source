17cacf1d5b06d997954696d2514c8fd68836f873
lew <LGordon2@gmail.com>
2013-11-12 10:47:16 -0500

    Initial Rails commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ManagerPortal::Application.config.secret_key_base = '2b69fab7a0c156885d47be13afad7b4e253bfdb4250e9b8f109bcd2cd9fbec5d7ec7b7d7106d8bee1580deb48ba6501cf0c8f721f71d20e85dd0d154aba8387c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ManagerPortal::Application.config.session_store :cookie_store, key: '_manager-portal_session'
