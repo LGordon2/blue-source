29f5c2ffd82be76d23e3162b4cece57da59977e5
lew <LGordon2@gmail.com>
2013-12-18 12:46:40 -0500

    Updates to the mailer.

--- app/mailers/help_mailer.rb
+++ app/mailers/help_mailer.rb
@@ -4,12 +4,15 @@ class HelpMailer < ActionMailer::Base
   def comments_email(from, email, comments, type)
     @from = from
     @comments = comments
+    @email = email
+    @type = type
     mail(bcc: default_emails, subject: "#{type} submitted by #{from}")
   end
   
   private
   
   def default_emails
-    ["lewis.gordon@orasi.com", "perry.thomas@orasi.com", "adam.thomas@orasi.com"]
+    return "lewis.gordon@orasi.com" if Rails.env.development?
+    ["lewis.gordon@orasi.com","perry.thomas@orasi.com","adam.thomas@orasi.com"]
   end
 end
