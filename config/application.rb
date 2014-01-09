3094a26b03bffa8aaaf313325df648d99a09924b
lew <LGordon2@gmail.com>
2014-01-09 08:50:36 -0500

    Change to application to include all assets for precompilation.

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -20,5 +19,20 @@ module ManagerPortal
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += Dir["#{Rails.root}/lib/ext"]
+    config.assets.precompile << Proc.new do |path|
+      if path =~ /\.(css|js)\z/
+        full_path = Rails.application.assets.resolve(path).to_path
+        app_assets_path = Rails.root.join('app', 'assets').to_path
+        if full_path.starts_with? app_assets_path
+          puts "including asset: " + full_path
+        true
+        else
+          puts "excluding asset: " + full_path
+        false
+        end
+      else
+      false
+      end
+    end
   end
 end
