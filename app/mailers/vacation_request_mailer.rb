ceef70d4072ea64e376b48e288aad13d420ed72e
lew <LGordon2@gmail.com>
2014-01-27 12:32:28 -0500

    Changes to accept/reject messages and better tables for managers and employees.  i.e. Removed extraneous cancel buttons.

--- app/mailers/vacation_request_mailer.rb
+++ app/mailers/vacation_request_mailer.rb
@@ -1,34 +1,42 @@
 class VacationRequestMailer < ActionMailer::Base
   default from: "mailer@orasi.com"
   
-  def request_email(current_user, to, cc=nil, vacation_params)
+  before_action :attach_logo
+  
+  def request_email(from_user, to_manager, vacation_params, cc=nil)
     if Rails.env.development?
-      to = "lewis.gordon@orasi.com"
+      to_manager.email = "lewis.gordon@orasi.com"
       cc = "lewis.gordon@orasi.com"
     end
     if Rails.env.staging?
-      to = "bluesourceqa@gmail.com"
+      to_manager.email = "bluesourceqa@gmail.com"
       cc = "bluesourceqa@gmail.com"
     end
-    
-    @current_user = current_user
+    @from_user = from_user
+    @to_manager = to_manager
     @vacation_params = vacation_params
-    attachments.inline['logo.png'] = File.read(Rails.root.to_path+"/app/assets/images/logo.png")
-    mail(to: to, cc: cc, subject: "[BlueSource] #{current_user.display_name}, #{vacation_params[:vacation_type]} Request")
+    
+    mail(to: @to_manager.email, cc: cc, subject: "[BlueSource] #{from_user.display_name}, #{vacation_params[:vacation_type]} Request")
   end
   
-  def confirm_email(current_user, to, vacation_params, accepted)
+  def confirm_email(from_manager, to_employee, vacation, accepted)
     if Rails.env.development?
-      to = "lewis.gordon@orasi.com"
+      to_employee.email = "lewis.gordon@orasi.com"
     end
     if Rails.env.staging?
-      to = "bluesourceqa@gmail.com"
+      to_employee.email = "bluesourceqa@gmail.com"
     end
-
+    @to_employee = to_employee
+    @from_manager = from_manager
+    @vacation = vacation
     @accepted = accepted
-    @current_user = current_user
-    @vacation_params = vacation_params
     @confirm_status = accepted ? "Accepted" : "Rejected"
-    mail(to: to, subject: "[BlueSource] #{current_user.display_name}, #{vacation_params[:vacation_type]} #{@confirm_status.capitalize}")
+    mail(to: @to_employee.email, subject: "[BlueSource] #{@from_manager.display_name}, #{@vacation.vacation_type} #{@confirm_status.capitalize}")
+  end
+  
+  private
+  
+  def attach_logo
+    attachments.inline['logo.png'] = File.read(Rails.root.to_path+"/app/assets/images/logo.png")
   end
 end
