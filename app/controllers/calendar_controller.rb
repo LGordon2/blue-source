d57cd73b919df5d8e1fdef6e7a35d9f0c402a47a
lew <LGordon2@gmail.com>
2014-02-04 10:00:01 -0500

    Renamed absent to calendar.

---
+++ app/controllers/calendar_controller.rb
@@ -0,0 +1,14 @@
+class CalendarController < ApplicationController
+  before_action :require_manager_login
+  
+  def index
+    @all_months = (1..12).collect {|month_no| [Date.new(2014,month_no,1).strftime("%B"), month_no]}
+    unless params[:year].blank? or params[:month].blank?
+      @starting_date = Date.new(params[:year].to_i,params[:month].to_i,1)
+    else
+      @starting_date = Date.current.change(day: 1)
+    end
+    @pdo_times = Vacation.where("start_date >= ? and start_date <= ?",@starting_date.beginning_of_month,@starting_date.end_of_month)
+  end
+  
+end
