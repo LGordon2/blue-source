e4a6395749066febfc86c9c36ba244b6703d3d4f
lew <LGordon2@gmail.com>
2014-02-06 09:21:12 -0500

    More updates to department dropdowns before Disney call.

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,6 +3,8 @@ class Department < ActiveRecord::Base
   belongs_to :parent_department, class_name: "Department", foreign_key: "department_id"
   has_many :top_level_employees, class_name: "Employee"
   
+  validates :name, uniqueness: {case_sensitive: false}
+  
   def employees
     Employee.where(id: self.top_level_employees.pluck(:id) + sub_departments.map do |sub_dept|
       [sub_dept.top_level_employees.pluck(:id)] + sub_dept.employees
@@ -19,5 +21,13 @@ class Department < ActiveRecord::Base
     end
     
     self.above? other_department.parent_department
+  end
+  
+  def find_path_to_highest_department
+    if self.parent_department.blank?
+      return [self.id]
+    end
+    
+    [self.id] + self.parent_department.find_path_to_highest_department
   end
 end
