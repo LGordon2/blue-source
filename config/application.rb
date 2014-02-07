0728730c7f2d1b81de2856ecd24e1a103f56dabb
lew <LGordon2@gmail.com>
2014-02-07 13:20:48 -0500

    Fixes hack for beginning of month/end of month.

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module ManagerPortal
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     I18n.enforce_available_locales = true
+    config.beginning_of_week = :sunday
     config.autoload_paths += Dir["#{Rails.root}/lib/ext"]
     config.assets.precompile << Proc.new do |path|
       if path =~ /\.(css|js|eot|svg|ttf|woff)\z/
