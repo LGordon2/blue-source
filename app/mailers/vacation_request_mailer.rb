473cf8ecece70ab6d4258ed4ed2293511f760e30
lew <LGordon2@gmail.com>
2014-02-11 15:55:53 -0500

    Removing fake email addresses for mailtrap.

--- app/mailers/vacation_request_mailer.rb
+++ app/mailers/vacation_request_mailer.rb
@@ -8,35 +8,24 @@ class VacationRequestMailer < ActionMailer::Base
       to_manager.email = "lewis.gordon@orasi.com"
       cc = "lewis.gordon@orasi.com"
     end
-    if Rails.env.staging? or Rails.env.heroku?
-      to_manager.email = "bluesourceqa@gmail.com"
-      cc = "bluesourceqa@gmail.com"
-    end
-    if Rails.env.production?
-      @memo = memo
-      @from_user = from_user
-      @to_manager = to_manager
-      @vacation = vacation
-  
-      mail(to: @to_manager.email, cc: cc, subject: "[BlueSource] #{from_user.display_name}, #{request_type(from_user, vacation)} Request")
-    end
+    @memo = memo
+    @from_user = from_user
+    @to_manager = to_manager
+    @vacation = vacation
+
+    mail(to: @to_manager.email, cc: cc, subject: "[BlueSource] #{from_user.display_name}, #{request_type(from_user, vacation)} Request")
   end
   
   def confirm_email(from_manager, to_employee, vacation, accepted)
     if Rails.env.development?
       to_employee.email = "lewis.gordon@orasi.com"
     end
-    if Rails.env.staging? or Rails.env.heroku?
-      to_employee.email = "bluesourceqa@gmail.com"
-    end
-    if Rails.env.production?
-      @to_employee = to_employee
-      @from_manager = from_manager
-      @vacation = vacation
-      @accepted = accepted
-      @confirm_status = accepted ? "Accepted" : "Rejected"
-      mail(to: @to_employee.email, subject: "[BlueSource] #{@from_manager.display_name}, #{request_type(to_employee, vacation)} #{@confirm_status.capitalize}")
-    end
+    @to_employee = to_employee
+    @from_manager = from_manager
+    @vacation = vacation
+    @accepted = accepted
+    @confirm_status = accepted ? "Accepted" : "Rejected"
+    mail(to: @to_employee.email, subject: "[BlueSource] #{@from_manager.display_name}, #{request_type(to_employee, vacation)} #{@confirm_status.capitalize}")
   end
   
   private
