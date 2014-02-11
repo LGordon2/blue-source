96fa6b76e0d89f0db64bbf3622c6f1c94517982f
lew <LGordon2@gmail.com>
2014-02-11 15:24:56 -0500

    Didn't work let's try this.

--- config/initializers/mailtrap.rb
+++ config/initializers/mailtrap.rb
@@ -1,6 +1,6 @@
 if ENV['MAILTRAP_HOST'].present?
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  ManagerPortal::Application.config.action_mailer.delivery_method = :smtp
+  ManagerPortal::Application.config.action_mailer.smtp_settings = {
     :user_name => ENV['MAILTRAP_USER_NAME'],
     :password => ENV['MAILTRAP_PASSWORD'],
     :address => ENV['MAILTRAP_HOST'],
