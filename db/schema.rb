9ef267516f727dca7b710d150f360fddee30fc38
lew <LGordon2@gmail.com>
2014-02-20 10:24:33 -0500

    Adding schema too...

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140218190942) do
+ActiveRecord::Schema.define(version: 20140220145851) do
 
   create_table "departments", force: true do |t|
     t.string   "name"
@@ -46,8 +46,8 @@ ActiveRecord::Schema.define(version: 20140218190942) do
     t.string   "location"
     t.integer  "department_id"
     t.text     "project_comments"
-    t.time     "scheduled_hours_start"
-    t.time     "scheduled_hours_end"
+    t.string   "scheduled_hours_start"
+    t.string   "scheduled_hours_end"
     t.integer  "title_id"
     t.text     "preferences"
     t.boolean  "sys_admin"
