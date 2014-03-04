7960d1f18e480d5af2620ae303a8d9c59f42ba77
lew <LGordon2@gmail.com>
2014-03-04 10:03:14 -0500

    Removing reporting from this branch.

--- config/routes.rb
+++ config/routes.rb
@@ -46,11 +46,7 @@ BlueSource::Application.routes.draw do
   
   resource :calendar, only: [:index] do 
      get '/', to: 'calendar#index'
-  end 
-  
-  resources :reports
-  
-  get 'stuff', to: 'reports#stuff'
+  end
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
