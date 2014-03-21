65f2fa5865fc9818cee14dcc4e370d2f8d492e04
lew <LGordon2@gmail.com>
2014-03-21 14:41:25 -0400

    Removed reference to team_lead_id.

--- app/models/project_lead.rb
+++ app/models/project_lead.rb
@@ -2,17 +2,5 @@ class ProjectLead < ActiveRecord::Base
   belongs_to :project
   belongs_to :employee
   
-  after_create :set_employees
-  
   validates :employee, uniqueness: { scope: :project, message: "can only be listed as lead once."}
-  
-  private
-  
-  def set_employees
-    if self.project.leads.count == 1
-      self.project.employees.each do |employee|
-        employee.update(team_lead: self.project.leads.first)
-      end
-    end
-  end
 end
