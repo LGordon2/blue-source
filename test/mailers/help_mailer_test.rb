18c6190639f703842a53142fa2f94c763b3f7b46
Lew Gordon <LGordon2@gmail.com>
2014-08-26 16:50:13 -0400

    Rubocop cleanup and covering contractor pdo request

--- test/mailers/help_mailer_test.rb
+++ test/mailers/help_mailer_test.rb
@@ -1,20 +1,20 @@
 require 'test_helper'
 
 class HelpMailerTest < ActionMailer::TestCase
-  test "mailer works" do
-    from = "Test User"
-    email = "test.user@orasi.com"
-    comments = "I hate blue source"
-    type = "Bug"
-    email = HelpMailer.comments_email(from, email, comments, type).deliver
+  test 'mailer works' do
+    from = 'Test User'
+    email = 'test.user@orasi.com'
+    comments = 'I hate blue source'
+    type = 'Bug'
+    HelpMailer.comments_email(from, email, comments, type).deliver
     assert !ActionMailer::Base.deliveries.empty?
   end
-  
-  test "login mailer works" do
-    from = "Test User"
-	email = "test.user@orasi.com"
-    comments = "I actually love blue source"
-    email = HelpMailer.login_help_email(from, email, comments).deliver
+
+  test 'login mailer works' do
+    from = 'Test User'
+    email = 'test.user@orasi.com'
+    comments = 'I actually love blue source'
+    HelpMailer.login_help_email(from, email, comments).deliver
     assert !ActionMailer::Base.deliveries.empty?
   end
 end
--- test/mailers/vacation_request_mailer_test.rb
+++ test/mailers/vacation_request_mailer_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class VacationRequestMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'contractor vacation set as reason for contractor vacation' do
+    from_user = employees(:contractor)
+    to_manager = employees(:manager)
+    vacation = vacations(:contractor_vacation)
+    memo = 'Testing'
+    VacationRequestMailer.request_email(from_user, to_manager, vacation, memo).deliver
+    assert !ActionMailer::Base.deliveries.empty?
+  end
 end
