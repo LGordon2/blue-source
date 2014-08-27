af80ca748ae189ced06783bdbaa13750de576f77
Lew Gordon <lgordon2@gmail.com>
2014-08-26 21:35:37 -0400

    Everything is now yellow or green...

--- test/models/department_test.rb
+++ test/models/department_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class DepartmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @consultant = employees(:consultant)
+    @rural = departments(:rural)
+    @services = departments(:services)
+    @sub_rural = departments(:sub_rural)
+  end
+
+  test 'smoke test for employees' do
+    assert_includes @rural.employees, @consultant
+    assert_includes @services.employees, @consultant
+  end
+
+  test 'find path from lower dept to most parent department' do
+    assert_equal @rural.find_path_to_highest_department, [@rural.id, @services.id]
+  end
+
+  test 'department cannot be its own parent department' do
+    @rural.parent_department = @rural
+    assert_not @rural.save
+  end
+
+  test 'parent association maintained upon deletion' do
+    assert @rural.parent_department == @services
+    assert @sub_rural.parent_department == @rural
+    Department.find(@rural.id).destroy
+    assert Department.find(@sub_rural.id).parent_department == @services
+  end
 end
